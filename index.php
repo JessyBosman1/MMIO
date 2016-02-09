d7ce6ce3556e059123b4ab9413816fa3b2dfabec
Jessy Bosman <je.bo@live.nl>
2016-02-09 14:13:31 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<link rel='stylesheet' href='Stylesheet.css' type='text/css'>
+
+<title>Jessy Bosman</title>
+<head>
+<h1>Portfolio
+Jessy Bosman - 11056045</h1>
+</head>
+<body>
+<ul>
+  <li>Home</li>
+  <li><a href="deliverables.php">Deliverables</a></li>
+  <li>Schetsen</li>  
+  <li>Over Mij</li>  
+<ul>
+</body>
+</html>
