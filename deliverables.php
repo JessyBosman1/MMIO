94797f6a9d71a40f0e715497d9f7f11e442fa29a
Jessy Bosman <je.bo@live.nl>
2016-02-09 14:12:48 +0100

    Create deliverables.php

---
+++ deliverables.php
@@ -0,0 +1,78 @@
+<html>
+<title>MMIO JBosman Deliverables</title>
+<head><h1>Deliverables</h1></head>
+<link rel='stylesheet' href='Stylesheet.css' type='text/css'>
+<?php include ('index.php');
+echo 'test';?>	
+<style>
+.inner_frame {
+border: 2px solid black; 
+width:33%;  
+padding:30px;
+}
+
+.outer_frame {
+display:none;
+}
+
+img{
+height:33%;
+width:100%
+}
+</style>
+
+<body>
+
+<script type="text/javascript">
+
+    function visibility(id) {
+       if(document.getElementById(id).style.display == 'block')
+          document.getElementById(id).style.display = 'none';
+       else
+          document.getElementById(id).style.display = 'block';
+    }
+
+</script>
+
+<button type="button" onclick="visibility('week1');">Week1</button>
+<div class="outer_frame" id="week1">
+<div class="inner_frame">
+
+<h2>Premiere opdracht 1:</h2>
+<br>
+<object data="https://www.youtube.com/v/Gers1QR9bfM" width="560" height="315"></object>
+<br>
+Kies één van de clips, en maak er een video bij. <br>
+laad hem in Premiere als audiotrack, en monteer er een collectie clips overheen.<br>
+Audio:<br>
+https://soundcloud.com/royalty-free-creative-commons/epic-trailer-cinematic-002-2royalty-free-creative-commons<br>
+Video:<br>
+https://www.youtube.com/watch?v=LBr7kECsjcQ<br>
+https://www.youtube.com/watch?v=QM0oVKi3Uug<br>
+
+</div>
+<div class="inner_frame">
+<h2>Portfolio Ontwerp1</h2>
+<img src="http://i64.tinypic.com/63ul2p.jpg" >
+</div>
+<div class="inner_frame">
+<h2>Portfolio Ontwerp2</h2>
+<img src="http://i68.tinypic.com/25ssmlj.jpg">
+</div>
+<div class="inner_frame">
+<h2>Portfolio Ontwerp3</h2>
+<img src="http://i67.tinypic.com/3089njm.jpg">
+</div>
+<div class="inner_frame">
+<h2>Uitwerking Portfolio ontwerp Photoshop</h2>
+<img src="http://i67.tinypic.com/2w1wepy.jpg">
+</div >
+<div class="inner_frame">
+<h2>Verslag ontwerp</h2>
+Het verslag over de ontwerpen en de uitleg daarbij. Er is gebruik gemaakt van pencil, Photoshop & InDesign.
+<a href="https://drive.google.com/file/d/0ByG7R3zPFDIQeWV2VDNNeEdxREk/view?usp=sharing">Link naar PDF verslag</a>
+</div>
+</div>
+
+</body>
+</html>
